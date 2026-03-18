b24563f22bcc0dfc0046d0decfe9eeca88592819
GurKalra <gurvanshsinghkalra@gmail.com>
2026-03-18 23:11:44 +0530

    feat: implemted 'Phase 8' rescue engine
    - Added universal prescient-rescue.sh script for chroot recovery.
    - Added Ubuntu (initramfs-tools) and Arch (mkinitcpio) injection hooks.
    - Updated hooks.py to dynamically install and rebuild the kernel RAM disk.

---
+++ src/prescient/initramfs/prescient-rescue.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "=========================================="
+echo "    PRESCIENT EMERGENCY RESCUE SYSTEM     "
+echo "=========================================="
+
+# Checking for real root filesystem
+if [ -d "/root/etc" ];then
+    echo "[+] Real root filesystem detected at /root"
+    echo "[+] Preparing to enter chroot environment..."
+
+    # Bind critical virtual filesystems
+    mount -t proc proc /root/proc
+    mount -t sysfs sys /root/sys
+    mount -o bind /dev /root/dev
+
+    echo "[+] Waking up Prescient Rollback Engine...'
+    chroot /root /bin/bash -c "prescient undo"
+
+    echo "=========================================="
+    echo "If the rollback was successful, type 'reboot' or press Ctrl+Alt+Del."
+else
+    echo "[-] ERROR: Root filesystem is not mounted at /root."
+    echo "[-] Prescient cannot automatically locate your hard drive in this state."
+    echo "[-] Please mount your root partition manually to /root, then run 'prescient-rescue'."
+fi
